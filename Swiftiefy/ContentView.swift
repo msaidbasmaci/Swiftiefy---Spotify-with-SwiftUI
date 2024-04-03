b65ebdbe30ddb0a948971dde7caadb6e4aeaf26d
msaidbasmaci <muhammedbsmcm@gmail.com>
2024-04-03 19:00:11 +0300

    Initial Commit

---
+++ Swiftiefy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftiefy
+//
+//  Created by Muhammed Said BASMACI on 3.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftiefy/SwiftiefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftiefyApp.swift
+//  Swiftiefy
+//
+//  Created by Muhammed Said BASMACI on 3.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftiefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
