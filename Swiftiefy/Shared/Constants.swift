4c0dd6f413bee7e8ad86cb49854f95de877f621a
msaidbasmaci <muhammedbsmcm@gmail.com>
2024-04-04 21:34:19 +0300

    Developing the project

---
+++ Swiftiefy/Shared/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Swiftiefy
+//
+//  Created by Muhammed Said BASMACI on 4.04.2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
---
+++ Swiftiefy/Shared/ImageLoaderView.swift
@@ -0,0 +1,35 @@
+//
+//  ImageLoaderView.swift
+//  Swiftiefy
+//
+//  Created by Muhammed Said BASMACI on 4.04.2024.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    var urlString: String = Constants.randomImage
+    var resizingMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0.001)
+            .overlay(
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .cornerRadius(30)
+        .padding(40)
+        .padding(.vertical,60)
+}
